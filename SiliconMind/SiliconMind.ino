c229cde60c68e5ec14b711d1e103874746baccaf
Andy <aecobley@dundee.ac.uk>
2017-11-19 12:15:34 +0000

    Towards PolyPhonic

--- SiliconMind/SiliconMind.ino
+++ SiliconMind/SiliconMind.ino
@@ -1,23 +1,27 @@
 #include "SPI.h"
-#define MONO 1;
-#define SPLIT 2;
+#define MONO 1
+#define SPLIT 2
+#define POLY 3
+
+const int MaxPoly=2;
 
 int ScanOut[8] = {2, 3, 4, 5, 6, 7, 8, 9};
 int ScanIn[5] = {A0, A1, A2, A3, A4};
-int GateOut[2] = {0, 1};
+int DACS[2] = {10, 1};
 
 int Range = 819; // (2^12/5)
 int Vss = 5;
 
-int KeyPressed[4] = { -1, -1, -1, -1};
-int DACS[2] = {10, 1};
+int KeyPressed[MaxPoly];
+
 byte Key = -1;
 byte Octave = -1;
 byte Note = -1;
 int outValue = 0;
 
 int mode = SPLIT;
-boolean States[2];
+boolean States[MaxPoly];
+int GateOut[MaxPoly] = {0, 1};
 
 void setup() {
   //Serial.begin(9600);
@@ -28,45 +32,61 @@ void setup() {
   for (int i = 0; i < 5; i++) {
     pinMode(ScanIn[i], INPUT);
   }
-  for (int i = 0; i < 2; i++) {
+  for (int i = 0; i < MaxPoly; i++) {
     pinMode(GateOut[i], OUTPUT);
     digitalWrite(GateOut[i], false);
     pinMode(DACS[i], OUTPUT); //DAC Chip Select
     States[i] = false;
+    KeyPressed[i]=-1;
   }
   SPI.begin();
   SPI.setBitOrder(MSBFIRST);
 }
 
 void loop() {
-
+  int CurrentFinger = 0;
   for (int i = 0; i < 8; i++) {
     digitalWrite(ScanOut[i], true);
     for (int j = 0; j < 5; j++) {
       int in = digitalRead(ScanIn[j]);
-      if (in == 1) {  // If any key is pressed set the gate out to true and set a flag (State)
-        States[0] = true;
-        digitalWrite(GateOut[0], true); // this only works in monophonic mode
-      }
       Key = 8 * j + i;
+      if (mode==SPLIT){
+        if (Key<13){
+          CurrentFinger=1;
+        }
+      }
+
+      if (in == 1) {  // If any key is pressed set the gate out to true and set a flag (State)
+        States[CurrentFinger] = true;
+        digitalWrite(GateOut[CurrentFinger], true); // this only works in monophonic mode
+      }
+      
       if ((in != 0) &&    //A key is pressed
-          (Key != KeyPressed[0]) ) { //It's not equal to the current key (we only need to write out once)
+          (Key != KeyPressed[CurrentFinger]) ) { //It's not equal to the current key (we only need to write out once)
 
-        KeyPressed[0] = Key;       //Record this Key
+        KeyPressed[CurrentFinger] = Key;       //Record this Key
 
+      }
+      if (mode==POLY){
+        CurrentFinger++;
+        if (CurrentFinger >=MaxPoly)
+           CurrentFinger=MaxPoly;
       }
     }
     digitalWrite(ScanOut[i], false);
   }
-  if (States[0] == false) {  //No Key was pressed this time round
-    KeyPressed[0] = -1;        //Set the Gate to a default value
-    digitalWrite(GateOut[0], false);
-  } else {
-    Key = KeyPressed[0];
-    Octave = (byte)(Key / 12);
-    Note = (byte)(Key % 12);
-    outValue = Range * (Octave + (float)Note / 12);
-    mcpWrite(outValue, 0); //Send the value to the  DAC
+  for (int i = 0; i < 2; i++) {
+    CurrentFinger = i;
+    if (States[CurrentFinger] == false) {  //No Key was pressed this time round
+      KeyPressed[CurrentFinger] = -1;        //Set the Gate to a default value
+      digitalWrite(GateOut[CurrentFinger], false);
+    } else {
+      Key = KeyPressed[CurrentFinger];
+      Octave = (byte)(Key / 12);
+      Note = (byte)(Key % 12);
+      outValue = Range * (Octave + (float)Note / 12);
+      mcpWrite(outValue, CurrentFinger); //Send the value to the  DAC
+    }
   }
 }
 
